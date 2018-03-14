7d68d50ac47df369b47ce70401edf8ad60d48b4c
Jean luc <you@example.com>
2018-03-14 11:00:43 +0100

    synchro

--- bootstrap.js
+++ bootstrap.js
@@ -4,3 +4,13 @@ function Khi()
 }
 
 
+Khi.isFunction = function(functionToCheck)
+{
+    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+};
+
+
+Khi.pad=function(n)
+{
+    return n<10 ? '0'+n : n
+};
